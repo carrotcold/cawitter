c1d8962d19f7dbb96c0cbd463f4c5dbef4a86e78
carrotcold <carrotcold@gmail.com>
2020-09-28 23:53:27 +0900

    #2.1 Login Form part One

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import AppRouter from 'components/Router';
+import { auth } from 'myFirebase';
 
 function App() {
-  return <div></div>;
+  // https://firebase.google.com/docs/reference/js/firebase.auth.Auth?authuser=0
+  // auth.currentUser: User | null
+  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Cawitter</footer>
+    </>
+  );
 }
 
 export default App;
